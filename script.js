5597c156186fd748dcdf34992a89388be209512f
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-04-04 06:34:21 -0300

    Bonus task done

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const NULL_EMPTY_OR_BLANK_REGEX = /^\s*$/;
+let planets = [];
 
-var planets = [];
-
-document.addEventListener('DOMContentLoaded', function(){ set_planets() })
+document.addEventListener('DOMContentLoaded', function() { set_planets() })
 
 async function getAllPlanets(url, allPlanets = [])
 {
-  const data = await (await fetch(url)).json();
-  allPlanets.push(...data.results);
+  const DATA = await (await fetch(url)).json();
+  
+  allPlanets.push(...DATA.results);
 
-  if (data.next) 
-    return getAllPlanets(data.next, allPlanets);
+  if (DATA.next) 
+    return getAllPlanets(DATA.next, allPlanets);
   
   return allPlanets;
 }
@@ -24,9 +24,7 @@ function findPlanets()
     for (let counter = 0; counter < planets.length; counter++)
     {
       if (planets[counter].name === search_field.value.trim())
-      {
-        return selectPlanet(counter);
-      }     
+        return selectPlanet(counter); 
     }
 
     alert("Planeta não encontrado!");
@@ -35,28 +33,59 @@ function findPlanets()
 
 async function set_planets()
 {
-  planets = await getAllPlanets('https://swapi.dev/api/planets')
+  planets = await getAllPlanets('https://swapi.dev/api/planets');
+  const PLANET_LIST = document.getElementById('planet_list');
 
   planets.forEach((planet) =>
   {
     let li = document.createElement("li");
 
     li.innerHTML =
-    `<button onclick="selectPlanet(${planets.indexOf(planet)})">${planet.name}</button>`
+    `
+      <button onclick="selectPlanet(${planets.indexOf(planet)})">
+        ${planet.name}
+      </button>
+    `;
 
-    planet_list.appendChild(li);
+    PLANET_LIST.appendChild(li);
   })
 
   console.log(planets);
 }
 
-function selectPlanet(id)
+async function selectPlanet(id)
 {
-  article = document.getElementById('planet_description');
-
-  article.innerHTML = `
-    <h1>${planets[id].name}</h1>
+  document.getElementById('planet_description').innerHTML =
+  `
+    <h2>${planets[id].name}</h2>
     <p>Clima: ${planets[id].climate}</p>
     <p>População: ${planets[id].population}</p>
-    <p>Tipo de Terreno: ${planets[id].terrain}</p>`
+    <p>Tipo de Terreno: ${planets[id].terrain}</p>
+    
+    <h3>Habitantes Famosos</h3>
+    <table>
+      <thead>
+        <th>Nome</th>
+        <th>Data de Nascimento</th>
+      </thead>
+      <tbody id="resident_rows">
+      </tbody>
+    </table>
+  `;
+
+  let resident_rows = document.getElementById('resident_rows');
+
+  planets[id].residents.forEach(async (resident_url) =>
+  {
+    const RESIDENT = await (await fetch(resident_url)).json()
+    let tr = document.createElement('tr');
+
+    tr.innerHTML =
+    `
+      <td>${RESIDENT.name}</td>
+      <td>${RESIDENT.birth_year}</td>
+    `;
+
+    resident_rows.appendChild(tr);
+  })
 }
