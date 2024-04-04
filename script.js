1ab43c1170c2dc169b0398d7074fcb40e44145a3
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-04-04 05:05:38 -0300

    Task 4 done

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const NULL_EMPTY_OR_BLANK_REGEX = /^\s*$/;
+
 var planets = [];
+
+document.addEventListener('DOMContentLoaded', function(){ set_planets() })
 
 async function getAllPlanets(url, allPlanets = [])
 {
@@ -6,19 +10,30 @@ async function getAllPlanets(url, allPlanets = [])
   allPlanets.push(...data.results);
 
   if (data.next) 
-  {
     return getAllPlanets(data.next, allPlanets);
-  }
   
   return allPlanets;
 }
 
-async function printAllPlanets()
+function findPlanets()
 {
-  console.log(await getAllPlanets('https://swapi.dev/api/planets'));
+  search_field = document.getElementById('search_field');
+
+  if (!NULL_EMPTY_OR_BLANK_REGEX.test(search_field.value))
+  {
+    for (let counter = 0; counter < planets.length; counter++)
+    {
+      if (planets[counter].name === search_field.value.trim())
+      {
+        return selectPlanet(counter);
+      }     
+    }
+
+    alert("Planeta não encontrado!");
+  }  
 }
 
-async function findPlanets()
+async function set_planets()
 {
   planets = await getAllPlanets('https://swapi.dev/api/planets')
 
@@ -31,6 +46,8 @@ async function findPlanets()
 
     planet_list.appendChild(li);
   })
+
+  console.log(planets);
 }
 
 function selectPlanet(id)
